bfbaf207f7ca0edef5a1f605cc278a545be186c4
weedavedev <admin@wallacedevelopment.co.uk>
2025-01-14 20:26:29 +0100

    updated the soial icons for production site - Refactored BaseImage to just Image as its fast becoming general image tag.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,19 +1,32 @@
 <script>
 	import '$lib/styles/global.css';
 	import '$lib/styles/footer.css';
+	import Image from '$lib/components/Image.svelte';
 </script>
 
 <footer class="bg-white py-4 mt-auto border-t border-gray-200">
 	<div class="container mx-auto px-4 text-center text-gray-600">
 		<div class="social-icons">
 			<a href="https://wa.me/447950020820" title="Contact us on WhatsApp">
-				<img src="/images/logos/whatsapp.png" alt="Contact us on WhatsApp" />
+				<Image
+					src="/images/logos/whatsapp.png"
+					alt="Contact us on WhatsApp"
+					className="social-icons-img"
+				/>
 			</a>
 			<a href="https://www.facebook.com/homefromhome101" title="Follow us on Facebook">
-				<img src="/images/logos/facebook.png" alt="Follow us on Facebook" />
+				<Image
+					src="/images/logos/facebook.png"
+					alt="Follow us on Facebook"
+					className="social-icons-img"
+				/>
 			</a>
 			<a href="https://www.instagram.com/dogshomefromhome/" title="Follow us on Instagram">
-				<img src="/images/logos/instagram.png" alt="Follow us on Instagram" />
+				<Image
+					src="/images/logos/instagram.png"
+					alt="Follow us on Instagram"
+					className="social-icons-img"
+				/>
 			</a>
 		</div>
 		<p>&copy; {new Date().getFullYear()} Home From Home. All rights reserved.</p>
